7108d35752205f548f5a2bd4b74e77a87115feee
root <root@linux28.go-west.at>
2020-08-25 17:35:40 +0200

    Changed DB fieldtype, cleaning

--- ext_tables.sql
+++ ext_tables.sql
@@ -4,7 +4,7 @@
 CREATE TABLE tt_content (
     tx_gowestconfig_gutter varchar(60) DEFAULT '' NOT NULL,
     tx_gowestconfig_summary varchar(255) DEFAULT '' NOT NULL,
-    tx_gowestconfig_hide_in_breakpoint text NOT NULL
+    tx_gowestconfig_hide_in_breakpoint varchar(255) DEFAULT '' NOT NULL
 );
 
 CREATE TABLE pages (
